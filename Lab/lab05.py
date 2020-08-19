26e6525aaf0ae11e0ad31f3200f3199cec95942f
teeradon43 <teeradonch43@gmail.com>
2020-08-19 17:32:55 +0700

    pls

--- Lab/lab05.py
+++ Lab/lab05.py
@@ -1,3 +1,52 @@
+'''
+    Week 2 Lab 5 by Lieutenant Sadder
+    Making MatchingGame : เกมต่อคำ
+    There are 3 function P R and X
+    P(item) use to chk if last 2 characters of previous word == first 2 characters of next words
+        then add the word to the list
+        if not the game will be over
+    R() use to Restart game
+    X() use to Exit the game 
+
+    Test Cases:
+    (1) *** TorKham HanSaa ***
+        Enter Input : P Apple,P LEMON,R,P onion,X
+        'Apple' -> ['Apple']
+        'LEMON' -> ['Apple', 'LEMON']
+        game restarted
+        'onion' -> ['onion']
+
+    (2) *** TorKham HanSaa ***
+        Enter Input : P apple,p lemon,P onion,X
+        'apple' -> ['apple']
+        'p lemon' is Invalid Input !!!
+
+    (3) *** TorKham HanSaa ***
+        Enter Input : P apPLE,P leMON,R,P Durian,P ant,P pineapple,X
+        'apPLE' -> ['apPLE']
+        'leMON' -> ['apPLE', 'leMON']
+        game restarted
+        'Durian' -> ['Durian']
+        'ant' -> ['Durian', 'ant']
+        'pineapple' -> game over
+
+    (4) *** TorKham HanSaa ***
+        Enter Input : R,R,P KK,R,P apple,R,P Lemon,R,X
+        game restarted
+        game restarted
+        'KK' -> ['KK']
+        game restarted
+        'apple' -> ['apple']
+        game restarted
+        'Lemon' -> ['Lemon']
+        game restarted
+
+    (5) *** TorKham HanSaa ***
+        Enter Input : P apple,P lemon,P nectarine,X
+        'apple' -> ['apple']
+        'lemon' -> ['apple', 'lemon']
+        'nectarine' -> game over
+'''
 class matchGame:
 
     def __init__(self):
@@ -11,7 +60,6 @@ class matchGame:
             return 0
 
     def P(self,item):
-        # self.words.append('One')
         if self.count > 0:
             if item[2:] in self.words: #Same Word
                 print("'"+item+"' is Invalid Input !!!")
@@ -46,7 +94,6 @@ print('*** TorKham HanSaa ***')
 game = matchGame()
 words = [c.strip() for c in (input('Enter Input : ').split(','))]
 for item in words:
-    # print(item,item.__contains__(' '))
     if game.chk(item) == 0:
         print("'"+item+"' is Invalid Input !!!")
         break
@@ -55,5 +102,3 @@ for item in words:
         quit()
     else:
         game.solve(item)
-
-# print(words)

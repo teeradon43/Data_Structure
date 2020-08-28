438db64dd0863d5c06e4e6c72c7a506f5fad3be9
teeradon43 <teeradonch43@gmail.com>
2020-08-28 15:48:57 +0700

    Update Stack

--- Algorithm/Stack.py
+++ Algorithm/Stack.py
@@ -1,3 +1,21 @@
 class Stack:
-    def __init__(self):
-        pass
+    def __init__(self,list = None):
+        if list == None:
+            self.items = []
+        else:
+            self.items = list
+
+    def push(self,i):
+        self.items.append(i)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def isEmpty(self):
+        return self.items == [] or len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
